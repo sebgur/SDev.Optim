1464ca6f238b6c4935e0ba884ac025f18cb8607f
sebgur <sebgur@gmail.com>
2021-03-07 16:13:34 +0800

    Adding solution.

---
+++ Apps/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SDev.Optim;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Optimizer.Check();
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+                Console.WriteLine(msg);
+            }
+        }
+    }
+}
---
+++ SDev.Optim/Optimizer/Optimizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDev.Optim
+{
+    public class Optimizer
+    {
+        public static void Check()
+        {
+            throw new Exception("OK then");
+        }
+    }
+}
